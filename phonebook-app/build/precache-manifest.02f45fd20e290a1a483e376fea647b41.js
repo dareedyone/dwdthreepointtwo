c596b391055cf969e94c902a0430dbaee6892b34
Ridwan Ijadunola <dareedyone@gmail.com>
2020-10-16 15:19:18 +0100

    swap to phonebook

---
+++ phonebook-app/build/precache-manifest.02f45fd20e290a1a483e376fea647b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1056067ac5ad6b8287ae74d3f1bdb3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc6d1708890f234b068",
+    "url": "/static/css/main.ace4d29c.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc6d1708890f234b068",
+    "url": "/static/js/main.c6417a49.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
